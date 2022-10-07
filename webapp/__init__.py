6613bdc1011bd5c8ee05364552a979267deb00e9
Kevin McCurley <github@digicrime.com>
2022-10-07 11:17:11 -0700

    Switch from celery to ThreadPoolExecutor with only one worker.

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -2,36 +2,60 @@
 This is used to manage the submission of final papers. Authors enter the site via
 an authenticated URL with a paper ID from hotcrp. When a paper is submitted, it
 receives an upload of a zip file with at least one tex file. We start a docker instance
-in a celery task to compile the output.
+in a separate task to compile the output.
 """
 
-from flask import Flask, request, render_template
+from collections import OrderedDict
+from flask import Flask, request, render_template, current_app
 from flask_mail import Mail
-# from flask_limiter import Limiter
-# from flask_limiter.util import get_remote_address
-from celery import Celery
+from concurrent.futures import ThreadPoolExecutor
+from enum import Enum
 import os
 import sys
-from config import Config
 
 # Make sure we aren't running on an old python.
 assert sys.version_info >= (3, 7)
 
+class StrEnum(str, Enum):
+    @classmethod
+    def from_str(cls, val):
+        """Convert string to enum value."""
+        for e in cls:
+            if e.value == val:
+                return e
+        return None
+
+class PaperStatus(StrEnum):
+    """Status of a paper."""
+    PENDING = 'PENDING'
+    CANCELLED = 'CANCELLED'
+    RUNNING = 'RUNNING'
+    FAILED_EXCEPTION = 'FAILED_EXCEPTION'
+    FAILED_COMPILE = 'FAILED_COMPILE'
+    COMPILED = 'COMPILED'
+    UNKNOWN = 'UNKNOWN'
+
+
 # Globally accessible SMTP client. This can be used in unit tests as well.
 mail = Mail()
-# Globally accessible rate limiter. This is initialized in create_app
-#limiter = Limiter(key_func=get_remote_address,
-#                  default_limits=[]) # default is no limit
-celery = Celery('webapp', broker='redis://:localhost:6379/0')
+
+# Used to keep track of compilations by paperid. When a task is
+# submitted, then the paperid key is added to this with a value of the Future
+# that will run the compilation.. When a task is finished it removes the paperid
+# from the task_queue. This allows us to keep track of which
+# papers are queued for processing so that we don't enqueue a paper
+# twice and monitor the status of the queue in the admin interface.
+task_queue = OrderedDict()
+
+executor = ThreadPoolExecutor(max_workers=1, thread_name_prefix='compiler')
+#executor.add_default_done_callback(_default_future_callback)
 
 def create_app(config):
     app = Flask('webapp', static_folder='static/', static_url_path='/')
     app.config.from_object(config)
 
     mail.init_app(app)
-    # limiter.init_app(app)
-    celery.conf.update(app.config)
-    
+    #executor.init_app(app)
     with app.app_context():
         # from admin import admin_routes
         from . import routes
--- webapp/tasks.py
+++ webapp/tasks.py
@@ -1,44 +1,43 @@
-"""This defines the celery tasks that will be run by the worker.
+"""This defines the tasks to be run in executor.
 """
-from celery import Celery
 import json
-import os
 from pathlib import Path
 import time
 from .compiler import runner
+from . import task_queue
 
-# TODO: how should we store this password?
-REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD',
-                                'go35HyTzf0rnowpl3ase')
-celery_app = Celery(__name__)
-celery_app.conf.broker_url = os.environ.get("CELERY_BROKER_URL",
-                                        f'redis://:{REDIS_PASSWORD}@localhost:6379/0')                                                                     
-celery_app.conf.result_backend = f'redis://:{REDIS_PASSWORD}@localhost:6379/0'
-celery_app.set_current()
-
-@celery_app.task(name="run_latex_task")
 def run_latex_task(input_path, output_path, paperid):
     """Execute latex on input_path contents, writing into output_path.
     args:
-       input_path: complete string path to input directory
-       output_path: complete string path to output directory
+       input_path: absolute string path to input directory
+       output_path: absolute string path to output directory
        paperid: string paperid
+    returns:
+       dict that contains 'error' if something goes wrong, and
+       'log', 'code', 'execution_time' if the compilation finishes.
+       A finished compilation may still contain errors if code!=0.
     """
+    output = {}
     try:
         start_time = time.time()
         output = runner.run_latex(input_path, output_path)
         end_time = time.time()
-        json_file = Path(output_path).parents[0] / Path('meta.json')
+        output['execution_time'] = end_time - start_time
+    except Exception as e:
+        output['error'] = 'Exception running latex: ' + str(e)
+    json_file = Path(output_path).parents[0] / Path('meta.json')
+    try:
         if json_file.is_file():
             jstr = json_file.read_text(encoding='UTF-8')
             data = json.loads(jstr)
-            data['execution_time'] = end_time - start_time
-            data['log'] = output['log']
-            data['code'] = output['code']
+            data['execution_time'] = output.get('execution_time', -1)
+            data['log'] = output.get('log', 'no log')
+            data['code'] = output.get('code', -1)
             json_file.write_text(json.dumps(data, indent=2), encoding='UTF-8')
         else:
             output['error'] = 'Missing json file.'
-        return json.dumps(output, indent=2)
     except Exception as e:
-        return 'Exception running latex: ' + str(e)
+        output['error'] = 'Exception saving output: ' + str(e)
+    task_queue.pop(paperid, None)
+    return json.dumps(output, indent=2)
 
