d3d4cc1b2fe72ac57313cca8105bea5009bc8db1
Kevin McCurley <github@digicrime.com>
2023-01-22 11:53:28 -0800

    Add some missing generic parameters.

--- webapp/config.py
+++ webapp/config.py
@@ -13,22 +13,26 @@ class Config:
     SECRET_KEY = 'removed_in_checked_in_version'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite'
     UPLOAD_FOLDER = '/tmp'
+    XAPIAN_DB_PATH = 'webapp/fundreg/xapian.db'
     DATA_DIR = 'webapp/data'
     SITE_NAME = 'IACR Publishing Pipeline'
     SITE_SHORTNAME = 'IACR CiC'
     USERS = None
+    ROOT_URL = 'http://localhost:5000'
 
 class ProdConfig(Config):
     FLASK_ENV = 'production'
     DEBUG = False
     TESTING = False
+    XAPIAN_DB_PATH = '/var/www/wsgi/latex-submit/webapp/fundreg/xapian.db'
     DATA_DIR = '/var/www/wsgi/latex-submit/webapp/data'
+    ROOT_URL = 'https://publish.iacr.org'
     
 class DebugConfig(Config):
     FLASK_ENV = 'development'
     DEBUG = True
     TESTING = True
-    USERS = [{'email': 'testing@iacr.org',
-              'password': 'myenigm@',
+    USERS = [{'email': 'testing@example.com',
+              'password': 'mypowers',
               'role': 'admin'}]
     
