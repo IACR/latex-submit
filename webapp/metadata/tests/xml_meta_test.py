cbe9a452b53814cfb61878422f9615d99b3ca794
Kevin McCurley <github@digicrime.com>
2024-02-19 21:08:05 -0800

    Move crossref to cic.iacr.org when the URLs are created.

--- webapp/metadata/tests/xml_meta_test.py
+++ webapp/metadata/tests/xml_meta_test.py
@@ -7,7 +7,7 @@ from lxml import etree
 sys.path.insert(0, '..')
 from compilation import Compilation, Meta
 from db_models import Journal
-from xml_meta import get_jats, get_crossref
+from xml_meta import get_jats
 sys.path.insert(0, '../..')
 from config import DebugConfig
 
@@ -121,39 +121,3 @@ def test_jats_example():
             print('error:' + str(e))
             print(schema.error_log)
         assert schema.validate(root) == True
-
-        
-def test_crossref_creation1():
-    schema = etree.XMLSchema(etree.parse('testdata/xml/schema/crossref/schema-0.3.1/schemas/crossref5.3.1.xsd'))
-    json_file = Path('testdata/xml/compilation1.json')
-    compilation = Compilation.model_validate_json(json_file.read_text(encoding='UTF-8', errors='replace'))
-    compilation.meta.URL = 'https://example.com/crossref'
-    journal = Journal({'EISSN': '1234-5678',
-                       'hotcrp_key': 'testkey',
-                       'name': 'Test Journal',
-                       'DOI_PREFIX': '10.1729',
-                       'acronym': 'CiC',
-                       'publisher': 'Society of Silly Books'})
-    xml = get_crossref('testbatch',
-                       'International Association for Testing',
-                       'testing@example.com',
-                       journal,
-                       [compilation])
-    authors = list(xml.iter('person_name'))
-    assert len(authors) == 5
-    affiliations = list(xml.iter('institution'))
-    assert len(affiliations) == 7 # institutions are spread through person_name elements.
-    citations = list(xml.iter('citation'))
-    assert len(citations) == 33
-    ET.indent(xml, space=' ', level=1)
-    xml_string =ET.tostring(xml, encoding='utf-8').decode('utf-8')
-    paras = xml.findall('.//{http://www.ncbi.nlm.nih.gov/JATS1}p')
-    print(paras)
-    assert len(paras) == 2
-    root = etree.fromstring(xml_string)
-    try:
-        schema.assertValid(root)
-    except Exception as e:
-        print('error:' + str(e))
-        print(schema.error_log)
-    assert schema.validate(root) == True
