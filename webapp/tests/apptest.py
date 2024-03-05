6f63325766506e8f62fbfd548c46e20a5bbddc45
Kevin McCurley <github@digicrime.com>
2024-03-04 16:12:43 -0800

    Fix a test.

--- webapp/tests/apptest.py
+++ webapp/tests/apptest.py
@@ -32,32 +32,12 @@ def temp_dir(request):
 @pytest.fixture(scope='module')
 def test_client(temp_dir):
     TestConfig.DATA_DIR = temp_dir / Path('data')
+    TestConfig.SQLALCHEMY_DATABASE_URI = 'sqlite:///{}/db.sqlite'.format(temp_dir)
+
     app = create_app(TestConfig)
     with app.test_client() as test_client:
         with app.app_context():
             yield test_client
-
-# @pytest.fixture(scope='module')
-# def test_client():
-#     with tempfile.mkdtemp() as temp_dir:
-        
-        
-#     flask_app = create_app(TestConfig)
-    
-#     # Create a test client using the Flask application configured for testing
-#     with flask_app.test_client() as testing_client:
-#         # Establish an application context
-#         with flask_app.app_context():
-#             yield testing_client  # this is where the testing happens!
-
-# @pytest.fixture
-# def temp_dir(request):
-#     temp_dir = tempfile.mkdtemp()
-#     data_dir = os.path.join(temp_dir, 'data')
-#     def teardown():
-#         shutil.rmtree(temp_dir)
-#     request.addfinalizer(teardown)
-#     return temp_dir
 
 # Very basic test to make sure the home page works.
 def test_home_page(test_client):
@@ -65,7 +45,8 @@ def test_home_page(test_client):
     assert response.status_code == 200
     assert b"IACR Communications in Cryptology" in response.data
 
-# Check for form
+# Check for SubmitForm to be populated when following a link
+# from hotcrp.
 def test_submit_form(test_client):
     args = {'paperid': 'testid',
             'hotcrp': 'cictest',
@@ -73,17 +54,22 @@ def test_submit_form(test_client):
             'version': Version.CANDIDATE.value,
             'submitted': '2024-02-29 14:22:05',
             'accepted': '2024-01-02 08:05:59',
+            'journal': 'cic',
+            'volume': '5',
             'issue': '3',
-            'volume': '5'}
+            'pubtype': PubType.ERRATA.value}
     key = TestConfig.HOTCRP_POST_KEY.encode('utf-8')
     args['auth'] = create_hmac([args['paperid'],
                                 args['hotcrp'],
                                 args['hotcrp_id'],
                                 args['version'],
                                 args['submitted'],
-                                args['accepted']],
+                                args['accepted'],
+                                args['journal'],
+                                args['volume'],
+                                args['issue'],
+                                args['pubtype']],
                                key=key)
-    args['pubtype'] = PubType.ERRATA.value
     args['errata_doi'] = '10.1729/feebar'
     url = '/submit?' + urlencode(args)
     response = test_client.get(url)
@@ -95,9 +81,10 @@ def test_submit_form(test_client):
     assert end > 0
     resp = resp[start:end]
     print(resp)
+    assert '<input class="form-control w-50" id="paperid" name="paperid" pattern="[-a-zA-Z0-9]{7,}" placeholder="paper ID" required type="hidden" value="testid">' in resp
     assert '<input id="hotcrp" name="hotcrp" required type="hidden" value="cictest">' in resp
     assert '<input id="hotcrp_id" name="hotcrp_id" required type="hidden" value="5">' in resp
-    assert '<input id="auth" name="auth" required type="hidden" value="f4be7f8030a3f1b43d5b06605db9f6b38bdbb78ba67cc41a69112759b2bacf6a">' in resp
+    assert '<input id="auth" name="auth" required type="hidden" value="53453dafa6da953c0c3859bb57f2c1473632594b9c8bc20cece9f339c9bd7d16">' in resp
     assert '<input id="version" name="version" required type="hidden" value="candidate">' in resp
     assert '<input id="submitted" name="submitted" required type="hidden" value="2024-02-29 14:22:05">' in resp
     assert '<input id="accepted" name="accepted" required type="hidden" value="2024-01-02 08:05:59">' in resp
